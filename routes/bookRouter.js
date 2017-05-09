a7db6c68b7189d17152d07366b65d407284f1347
nila <nilavanrajmani@gmail.com>
2017-05-09 14:09:11 +0530

    End of delete

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -24,15 +24,54 @@ var router = function (Book) {
             });
         });
 
+    //Middleware
+    bookRouter.use('/:bookId', function (req, res, next) {
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err)
+                res.send(500, err)
+            else if (book) {
+                req.book = book;
+                next();
+            }
+            else
+                res.send(404, 'Not found');
+        });
+    });
+
     bookRouter.route('/:bookId')
         .get(function (req, res) {
-
-            Book.findById(req.params.bookId, function (err, book) {
+            res.json(req.book);
+        })
+        .put(function (req, res) {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save(function (err) {
                 if (err)
-                    res.send(500, err)
+                    res.status(500, err).send();
                 else
-                    res.json(book);
+                    res.json(req.book);
             });
+        })
+        .patch(function (req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+
+            req.book.save(function (err) {
+                if (err)
+                    res.status(500, err).send();
+                else
+                    res.json(req.book);
+            });
+        })
+        .delete(function (req, res) {
+            req.book.remove();
+            res.status(204, 'Item Removed').send();
         });
     return bookRouter;
 
